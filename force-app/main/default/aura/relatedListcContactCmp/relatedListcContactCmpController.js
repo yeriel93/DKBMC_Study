242bc94054c9574a6f07de5de496d7f6d9a9cda0
Limyeonji <yeonji.lim@dkbmc.com>
2023-08-23 09:28:25 +0900

    Aura(related cContact) 데이터 불러오기 완료

--- force-app/main/default/aura/relatedListcContactCmp/relatedListcContactCmpController.js
+++ force-app/main/default/aura/relatedListcContactCmp/relatedListcContactCmpController.js
@@ -1,18 +1,13 @@
 ({
-    // 초기화 (related contact List 확인)
+    // 초기화 (Get related cContact List)
     fnInit : function(component, event, helper) {
         // 컬럼
         component.set('v.columns',[
             {label: 'Name', fieldName: 'Name', type: 'text'},
             {label: 'Title', fieldName: 'Title__c', type: 'text'},
-            {label: 'phone', fieldName: 'phone__c', type: 'text'},
+            {label: 'Phone', fieldName: 'Phone__c', type: 'text'},
         ]);
 
-        // apex에서 데이터 가져오기 (나중에는 helper에 구현하면 좋을 듯)
-        var getData = component.get('c.');
-        
-
-        component.set('v.iscContact', true);
-
+        helper.getInitData(component);
     }
 })
--- force-app/main/default/aura/relatedListcContactCmp/relatedListcContactCmpHelper.js
+++ force-app/main/default/aura/relatedListcContactCmp/relatedListcContactCmpHelper.js
@@ -1,5 +1,31 @@
 ({
-    helperMethod : function() {
+    getInitData : function(cmp) {
+        let action = cmp.get('c.getRelatedcContactList');
+        let recordId = cmp.get('v.recordId');
+        console.log('recordId = ', recordId);
+
+        action.setParams({'cAccountId' : recordId});
+        action.setCallback( this, function(response) {
+            if(response.getState()=='SUCCESS') {
+                // console.log('getReturnValue = ', response.getReturnValue());
+                let cContactList = response.getReturnValue();
+                cmp.set('v.data', cContactList);
+                
+                if(cContactList.length>0){
+                    cmp.set('v.iscContact', true);
+                } else {
+                    cmp.set('v.iscContact', false);
+                }
+            }
+            else if(response.getState()=='ERROR') {
+                console.log('getInitData ERROR =', response.getError()); 
+            }
+        });
+        
+        $A.enqueueAction( action ); //실행코드
+
+
+        
 
     }
 })
