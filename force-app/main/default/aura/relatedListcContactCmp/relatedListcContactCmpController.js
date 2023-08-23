79d9d76bf94ee589a0daf42f2a3e991df8df812d
Limyeonji <yeonji.lim@dkbmc.com>
2023-08-23 10:57:53 +0900

    rowaction 추가 apex class 이름 cAccount에서 cContact으로 변경

--- force-app/main/default/aura/relatedListcContactCmp/relatedListcContactCmpController.js
+++ force-app/main/default/aura/relatedListcContactCmp/relatedListcContactCmpController.js
@@ -1,13 +1,39 @@
 ({
     // 초기화 (Get related cContact List)
-    fnInit : function(component, event, helper) {
+    fnInit : function(cmp, event, helper) {
+        //rowaction
+        var actions = [
+            {label: "Edit", name: "edit"},
+            {label: "Delete", name: "delete"}
+        ];
+
         // 컬럼
-        component.set('v.columns',[
+        cmp.set('v.columns',[
             {label: 'Name', fieldName: 'Name', type: 'text'},
             {label: 'Title', fieldName: 'Title__c', type: 'text'},
             {label: 'Phone', fieldName: 'Phone__c', type: 'text'},
+            { type: 'action', typeAttributes: { rowActions: actions }},
         ]);
 
-        helper.getInitData(component);
-    }
+        helper.getInitData(cmp);
+
+        
+    },
+
+
+    handleRowAction : function(cmp, event, helper){
+        var action = event.getParam('action');
+        var row = event.getParam('row');
+
+        switch (action.name) {
+            case 'edit':
+                helper.udpateData(cmp, row);
+                break;
+
+            case 'delete':
+                helper.removeData(cmp, row);
+                break;
+        }
+    },
+
 })
--- force-app/main/default/aura/relatedListcContactCmp/relatedListcContactCmpHelper.js
+++ force-app/main/default/aura/relatedListcContactCmp/relatedListcContactCmpHelper.js
@@ -10,6 +10,7 @@
                 // console.log('getReturnValue = ', response.getReturnValue());
                 let cContactList = response.getReturnValue();
                 cmp.set('v.data', cContactList);
+                cmp.set('v.Listlength', cContactList.length);
                 
                 if(cContactList.length>0){
                     cmp.set('v.iscContact', true);
@@ -23,9 +24,19 @@
         });
         
         $A.enqueueAction( action ); //실행코드
+    },
 
-
+    //레코드 수정페이지로 이동
+    updateData: function(cmp, row){
         
+    },
 
-    }
+    //레코드 삭제
+    removeData: function(cmp, row){
+        // var rows = cmp.get('v.data');
+        // var rowIndex = rows.indexOf(row);
+
+        // rows.splice(rowIndex, 1);
+        // cmp.set('v.data', rows);
+    },
 })
