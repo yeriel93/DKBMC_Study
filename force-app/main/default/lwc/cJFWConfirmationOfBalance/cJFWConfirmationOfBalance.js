f4ceac44f94ffa7b55589d9f617dfaeed9005966
Limyeonji <yeonji.lim@dkbmc.com>
2023-08-28 16:15:33 +0900

    lwc로 잔고확인서 html 완료

--- force-app/main/default/lwc/cJFWConfirmationOfBalance/cJFWConfirmationOfBalance.js
+++ force-app/main/default/lwc/cJFWConfirmationOfBalance/cJFWConfirmationOfBalance.js
@@ -1,12 +1,19 @@
 import { LightningElement } from 'lwc';
+// image
+import IMAGE_RESOURCE from '@salesforce/resourceUrl/sample';
+import jsPDF from '@salesforce/resourceUrl/jsPDF';
+import { loadScript } from 'lightning/platformResourceLoader';
 
 export default class CJFWConfirmationOfBalance extends LightningElement {
+    imageUrl = IMAGE_RESOURCE;
     
-    connectedCallback(){
-        window.print();
+    renderedCallback(){
+        console.log(this.contact.data);
+        loadScript(this, jsPDF ).then(() => {});
+        if (this.jsPdfInitialized) {
+            return;
+        }
+        this.jsPdfInitialized = true;
     }
-    
-    generatePDF() {
-        window.print();
-    }
+
 }
