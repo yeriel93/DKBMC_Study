f93ca8a9f75bbfe908bc9992edcea283d947c670
Limyeonji <yeonjilim20@gmil.com>
2024-06-19 09:16:22 +0900

    구매요청에 필요한 검색바 그리기 완료

--- force-app/main/default/lwc/purchaseRequestModal/purchaseRequestModal.js
+++ force-app/main/default/lwc/purchaseRequestModal/purchaseRequestModal.js
@@ -2,14 +2,37 @@
  * @description       : 
  * @author            : yeonji.lim@dkbmc.com
  * @group             : 
- * @last modified on  : 17-06-2024
+ * @last modified on  : 18-06-2024
  * @last modified by  : yeonji.lim@dkbmc.com
 **/
 import { LightningElement } from 'lwc';
-import LightningModal from 'lightning/modal';
 
-export default class PurchaseRequestModal extends LightningModal {
-    
+const columns = [
+    { label: 'Label', fieldName: 'name' },
+    { label: 'Website', fieldName: 'website', type: 'url' },
+    { label: 'Phone', fieldName: 'phone', type: 'phone' },
+    { label: 'Balance', fieldName: 'amount', type: 'currency' },
+    { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
+];
+
+export default class PurchaseRequestModal extends LightningElement {
+
+    catecoryValue;
+    columns = columns;
+
+    get catecoryOptions() {
+        return [
+            { label: '필기류', value: 'C01' },
+            { label: '사무용품', value: 'C02' },
+            { label: '사무기기', value: 'C03' },
+        ];
+    }
+
+    /* 카테고리 변경 이벤트 */
+    handleCatecoryChange(event) {
+        this.value = event.detail.value;
+    }
+
     connectedCallback() {
         console.log('구매요청 모달 시작');
     }
